dcb442b57ece11be05f11677863ceaef150b7368
JTCubed <zeusio60@gmail.com>
2023-05-22 15:13:53 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point
+ * Return: Always 0
+ *
+ */
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-191\n");
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	exit(1);
 }
