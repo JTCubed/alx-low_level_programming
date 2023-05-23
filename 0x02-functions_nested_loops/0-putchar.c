98bd5a75e3530f52596138f836a5733b83c9af13
JTCubed <zeusio60@gmail.com>
2023-05-23 15:16:54 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	write(1, &c, 1);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
 	return (0);
 }
