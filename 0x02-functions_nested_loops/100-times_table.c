d934a4b94b0e7dc375b179615b51c20b053ee940
JTCubed <zeusio60@gmail.com>
2023-05-23 16:15:09 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,8 +36,8 @@ void print_times_table(int n)
 					} else if (product < 100)
 					{
 						_putchar(' ');
-						_putchar(product / 10 + '0');
 						_putchar(' ');
+						_putchar(product / 10 + '0');
 						_putchar(product % 10 + '0');
 					} else
 					{
