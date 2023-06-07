5c8848a0b4455a570279a098b49af5e9b5c87ae8
JTCubed <zeusio60@gmail.com>
2023-06-07 15:31:17 -0700

    file 101 added

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * wildcmp - returns 1 if s1 == s2 else 1
+ * @s1: string input
+ * @s2: string input 2
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (1);
+	}
+}
