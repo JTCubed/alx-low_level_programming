a34d1590030e0c1f3704beadde71a41692de811f
JTCubed <zeusio60@gmail.com>
2023-11-08 10:24:18 -0800

    file 0 added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,6 +2,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *tbl = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *tbl;
+	unsigned long int i;
+
+	tbl = malloc(sizeof(hash_table_t));
+	if (tbl == NULL)
+	{
+		free(tbl);
+		return(NULL);
+	}
+
+	tbl->size = size;
+	tbl->array = malloc(sizeof(hash_node_t *) * size);
+	if (tbl->array == NULL)
+	{
+		free(tbl->array);
+		return(NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		tbl->array[i] = NULL;
+	}
+
 	return (tbl);
 }
