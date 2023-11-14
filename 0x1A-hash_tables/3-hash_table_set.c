c6ee1f7a3bb273004a61923596919f5d05844f1d
JTCubed <zeusio60@gmail.com>
2023-11-14 05:18:54 -0800

    changes to file 3, hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,36 +10,41 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	new_node = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t)):
 	if (new_node == NULL)
 		return (0);
+
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	if (new_node ->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
 
-	if (ht->array[index] != NULL)
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-		return (1);
-	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 	hash_node_t *item;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || strlen(key) == 0 || *key == '\0')
 		return (NULL);
 
 	i = key_index((const unsigned char *)key, ht->size);
