12464f4adb2f58b3e9b586c4145de0ef9e1ea4d9
JTCubed <zeusio60@gmail.com>
2023-11-14 05:20:10 -0800

    hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t)):
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
