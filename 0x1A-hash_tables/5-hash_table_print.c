0975039169e3b9d692789972315462456752102e
JTCubed <zeusio60@gmail.com>
2023-11-14 06:01:48 -0800

    hot fix for files hash_table_print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,16 @@
 #include "hash_tables.h"
+/**
+ * hash_table_print - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ *
+ * Return: void
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	int printed = 0;
+
 	if (ht == NULL)
 		return;
 
