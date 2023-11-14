36dd48252e5b3eb818556c8c7991dac182d8e2da
JTCubed <zeusio60@gmail.com>
2023-11-14 05:59:04 -0800

    file 5 added

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,14 +4,12 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	int printed = 0;
-//	hash_node_t *node;
 	if (ht == NULL)
 		return;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-//		node = ht->array[i];
 		while (ht->array[i] != NULL)
 		{
 			if (printed)
