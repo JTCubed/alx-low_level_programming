e9d3a2a008e115ad152d7c24eb0fe27cc5ce4ae1
JT <107570310+JTCubed@users.noreply.github.com>
2024-07-15 19:49:41 +0200

    Delete 0x1E-search_algorithms directory

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-#include <stddef.h>
-#include <stdio.h>
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-void searching(int *array, size_t size);
-#endif
